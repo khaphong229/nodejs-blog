191a3a849d8de035eed52b62f52b4979f9c09ebb
khaphong229 <khaphong217@gmail.com>
2024-07-21 19:35:05 +0700

    update code

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const morgan = require('morgan')
+const path = require('path')
+const app = express()
+const port = 3000
+const hdb = require('express-handlebars')
+
+// Template Engine
+app.engine('hbs', hdb.engine({extname: '.hbs'}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resoureces/views'));
+
+// HTTP Logger
+app.use(morgan('combined'))
+app.use(express.static(path.join(__dirname, 'public')))
+app.get('/', (req, res) => {
+  res.render('home');
+})
+app.get('/news', (req, res) => {
+    res.render('news');
+  })
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`)
+})
+// Bản mới nhất mà vẫn chạy được, mời các bạn tham khảo
