f9b8be9c2185a577f222800096997b32b289098e
khaphong229 <khaphong217@gmail.com>
2024-07-31 14:28:27 +0700

    read from db

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,15 +1,22 @@
 const Course = require('../models/Course');
-
+const {multipleMongooseToObject} = require('../../util/mongoose')
 class SiteController {
     //[get] /
-    async index(req, res) {
-        try{
-            const courses = await Course.find({});
-            res.json(courses);
-        }catch(error){
-            console.log(error);
-        }
-        
+    // async index(req, res) {
+    //     try{
+    //         const courses = await Course.find({});
+    //         res.json(courses);
+    //     }catch(error){
+    //         console.log(error);
+    //     }
+    // }
+    index (req, res, next){
+        Course.find({})
+        .then(courses => {
+            // courses = courses.map(course => course.toObject())
+            res.render('home', { courses: multipleMongooseToObject(courses)})
+        })
+        .catch(next)
     }
     //[get] /search
     search(req, res) {
